22ab5ae14605c045d8381ee735c95d2acdad4739
Granc3k <mart.simon17@gmail.com>
2024-02-28 13:57:17 +0100

    alpha version

---
+++ script.js
@@ -0,0 +1,21 @@
+function openTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+function onLoad(){
+  openTab("bio");
+}
+
+function copyToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+}
